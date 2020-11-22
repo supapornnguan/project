b859925d434f349d9f61e638b0a885157cb35a31
supapornnguan <supaporn.ngu@student.mahidol.edu>
2020-11-22 16:11:26 +0700

    add new file client

---
+++ client/src/components/loginSeller.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="loginUser">
+        <h1 id="login">LOG IN</h1>
+        <sui-form>
+            <sui-form-field>
+                <label id="labelLogin">EMAIL</label>
+                <input placeholder="EMAIL" v-model="seller_emial"/>
+            </sui-form-field>
+            <sui-form-field>
+                <label id="labelLogin">PASSWORD</label>
+                <input placeholder="PASSWORD" v-model="seller_password"/>
+            </sui-form-field>
+         
+            <sui-button  @click="login" style="margin-left:150px">Login</sui-button>
+        </sui-form>
+    </div>
+</template>
+
+<script>
+// import firebase from "../firebase"
+export default {
+    data() {
+        return {
+            seller_emial :'',
+            seller_password :''
+        }
+    },
+}
+</script>
+
+<style>
+.loginUser{
+  width: 400px;
+  margin: 0 auto;
+  margin-top: 100px;
+}
+#login{
+    margin-bottom: 50px;
+    text-align: center;
+}
+#labelLogin{
+    text-align: left;
+}
+
+</style>
---
+++ client/src/components/myOrders.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <sui-menu pointing>
+      <a
+        is="sui-menu-item"
+        v-for="item in items"
+        :active="isActive(item)"
+        :key="item"
+        :content="item"
+        @click="select(item)"
+      />
+      <sui-menu-menu position="right">
+        <sui-menu-item>
+          <sui-input transparent icon="search" placeholder="Search..." />
+        </sui-menu-item>
+      </sui-menu-menu>
+    </sui-menu>
+
+    <sui-segment>
+      <!-- ALL -->
+      <myOrdersAll v-if="active === 'ALL'">
+      </myOrdersAll> 
+      <!-- PICKUP -->
+      <myOrdersPickup v-if="active === 'PICKUP'">
+      </myOrdersPickup>
+      <!-- SHIPPING -->
+      <myOrdersShipping v-if="active === 'SHIPPING'">
+      </myOrdersShipping>
+    </sui-segment>
+  </div>
+</template>
+
+<script>
+import myOrdersAll from "../components/myOrdersAll"
+import myOrdersPickup from "../components/myOrdersPickup"
+import myOrdersShipping from "../components/myOrdersShipping"
+
+export default {
+  name: 'PointingMenuExample',
+  data() {
+    return {
+      active: 'ALL',
+      items: ['ALL', 'PICKUP', 'SHIPPING'],
+    };
+  },
+  methods: {
+    isActive(name) {
+      return this.active === name;
+    },
+    select(name) {
+      this.active = name;
+    },
+  },
+  components:{
+    myOrdersAll,
+    myOrdersPickup,
+    myOrdersShipping
+  }
+};
+</script>
