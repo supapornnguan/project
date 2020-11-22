24135197ca34cd4c9f9eddc4c37f5f0dbabc267d
supapornnguan <supaporn.ngu@student.mahidol.edu>
2020-11-22 16:49:30 +0700

    add (pickup.vue, searchStore, sellerLogin, sellerRegister)

---
+++ client/src/views/sellerLogin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <navbar/>
+        <loginSeller/>
+    </div>
+</template>
+<script>
+import navbar from "../components/navbar"
+import loginSeller from "../components/loginSeller"
+export default {
+    components : {
+        loginSeller,
+        navbar
+    }
+}
+</script>
